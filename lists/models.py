4fb9d9ac516c716e2309fdc22184bd722c5bc4c2
user <getbotcc@yandex.ru>
2020-09-18 14:27:26 +0400

    Модель для элементов списка и связанные миграции

--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Item(models.Model):
+    text = models.TextField(default='')
+
+    def __str__(self):
+        """строковое представление модели"""
+        return self.text
--- lists/tests.py
+++ lists/tests.py
@@ -3,12 +3,13 @@ from django.template.loader import render_to_string
 from django.test import TestCase
 from django.urls import resolve
 from .views import home_page
+from .models import Item
 
 # Create your tests here.
 
 
 class HomePageTest(TestCase):
-    """тест на токсичность"""
+    """тест домашней страницы"""
 
     def test_home_page_returns_correct_html(self):
         """тест: домашняя страница возвращает правильный html"""
@@ -25,3 +26,24 @@ class HomePageTest(TestCase):
         response = self.client.post('/', data={'item_text': 'a new list item'})
         self.assertIn('a new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
+
+
+class ItemModelTest(TestCase):
+    """тест модели Item"""
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'The second item'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first (ever) list item')
+        self.assertEqual(second_saved_item.text, 'The second item')
