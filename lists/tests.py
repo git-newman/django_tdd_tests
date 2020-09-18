00b855cbfabfb5ed724b8f8c44c6fe682cd61449
user <getbotcc@yandex.ru>
2020-09-18 08:14:47 +0400

    первый модульный тест с фиктивной home_page

--- lists/tests.py
+++ lists/tests.py
@@ -1,11 +1,14 @@
 from django.test import TestCase
+from django.urls import resolve
+from .views import home_page
 
 # Create your tests here.
 
 
-class SmokeTest(TestCase):
+class HomePageTest(TestCase):
     """тест на токсичность"""
 
-    def test_bad_math(self):
-        """неправильные математические расчеты"""
-        self.assertNotEqual(1+4, 3)
+    def test_root_url_resolves_to_home_page_view(self):
+        """тест: корневой url преобразуется в представление домашней страницы"""
+        found = resolve('/')
+        self.assertEqual(found.func, home_page)
